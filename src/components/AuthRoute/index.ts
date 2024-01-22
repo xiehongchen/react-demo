2cfd4fe9b516b739f1de5f7ea3cb2c0026d20493
xiehongchen <1754581057@qq.com>
2024-01-22 15:51:46 +0800

    修改

--- src/components/AuthRoute/index.ts
+++ src/components/AuthRoute/index.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: liaozhaozhou liaozhaozhou@ecyao.com
  * @Date: 2024-01-22 15:37:47
- * @LastEditors: liaozhaozhou liaozhaozhou@ecyao.com
- * @LastEditTime: 2024-01-22 15:46:23
+ * @LastEditors: xiehongchen 1754581057@qq.com
+ * @LastEditTime: 2024-01-22 15:51:35
  * @FilePath: /react-demo/src/components/AuthRoute/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,7 +18,6 @@ import { ReactNode } from 'react';
 interface AuthRouteProps {
   children: ReactNode;
 }
-
 function AuthRoute ({ children }: AuthRouteProps): JSX.Element {
   const isToken = getToken()
   return isToken ? <>{children}</> : <Navigate to="/login" replace />;
